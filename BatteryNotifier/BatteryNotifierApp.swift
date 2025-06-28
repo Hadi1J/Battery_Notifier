281bf9c99f33b443b199414314fc4e64c46a6bf8
Hadi Direya <hadisamer1420@gmail.com>
2025-06-28 18:45:09 +0300

    Initial Commit

---
+++ BatteryNotifier/BatteryNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryNotifierApp.swift
+//  BatteryNotifier
+//
+//  Created by Hadi on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BatteryNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
